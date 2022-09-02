d06c37d80d91feb3f7b162a3e769c91bcc06762f
Phil Todisco <philtodisco@phils-mbp.lan>
2022-09-02 16:00:52 -0400

    Display array from API as UL

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 const showAllBtn = document.getElementById('show-all')
+const breweryList = document.getElementById('brewery-list')
+
+function displayData(data) {
+    data.forEach(brewery => {
+        let breweryListItem = document.createElement("li")
+    breweryListItem.classList.add("list-item")
+        breweryListItem.textContent += brewery.name
+        breweryList.appendChild(breweryListItem)
+        console.log(brewery.name)
+    })
+}
 
 function getAllBreweries() {
     fetch('https://api.openbrewerydb.org/breweries')
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+        displayData(data)
+
+    })
 }
 
 showAllBtn.addEventListener('click', getAllBreweries)
